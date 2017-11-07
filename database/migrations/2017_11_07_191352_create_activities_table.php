d17625d651e2f29be7a5f4c3521469c4c49f5216
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-11-07 14:31:17 -0500

    Add initial table schema

--- database/migrations/2017_11_07_191352_create_activities_table.php
+++ database/migrations/2017_11_07_191352_create_activities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateActivitiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->binary('avatar')->nullable();
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->time('time_start')->nullable();
+            $table->time('time_end')->nullable();
+            $table->text('description');
+            $table->string('location')->nullable();
+            $table->binary('image')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('activities');
     }
 }
