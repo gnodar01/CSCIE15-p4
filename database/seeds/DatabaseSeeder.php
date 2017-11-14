c06c8e7a7d173a665ce03ab27d7371ccdf80e6de
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-11-14 00:41:36 -0500

    Add Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ActivitiesTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(GroupsTableSeeder::class);
     }
 }
