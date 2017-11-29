9e07eba41731eb95596ea054d493af7f1e831aeb
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-11-29 14:37:48 -0500

    Add debugbar

--- app/Http/Controllers/ActivitusController.php
+++ app/Http/Controllers/ActivitusController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App;
+use App\Activity;
 
 class ActivitusController extends Controller
 {
@@ -15,6 +16,12 @@ class ActivitusController extends Controller
      * @return main Digest view
      */
     public function index() {
-        return view('activitus.index');
+        $activities = Activity::all();
+
+        echo $activities;
+
+        return view('activitus.index')->with([
+            'activities' => $activities
+        ]);
     }
 }
