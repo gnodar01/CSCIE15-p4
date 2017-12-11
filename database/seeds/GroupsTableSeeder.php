8e1884bd4b9e8763d335166bcf374787005a6e7a
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-11 17:09:45 -0500

    Update Groups Seeder to have a group per activity

--- database/seeds/GroupsTableSeeder.php
+++ database/seeds/GroupsTableSeeder.php
@@ -13,7 +13,8 @@ class GroupsTableSeeder extends Seeder
     public function run()
     {
         $groups = [
-            ['Runenr Krew', 'We love to run!']
+            ['Outside Activities', 'We love to run, hike, camp, and play outside!'],
+            ['Happy Fun Time', 'Mostly drinking...']
         ];
 
         $count = count($groups);
