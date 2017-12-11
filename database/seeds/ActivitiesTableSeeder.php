2b3323639ff3b04b2491f4a3599efff16d918e6a
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-11 17:12:17 -0500

    Update activities seed to include group id

--- database/seeds/ActivitiesTableSeeder.php
+++ database/seeds/ActivitiesTableSeeder.php
@@ -13,15 +13,18 @@ class ActivitiesTableSeeder extends Seeder
     public function run()
     {
         $activities = [
-            ['Hiking', '11-07-2017', '11-07-2017', '08:00:00', '14:00:00', 'Hike before thanksgiving', 'pre-turkey hike'],
-            ['Fun Run', '12-01-2017', '12-01-2017', '08:00:00', '15:00:00', 'A Fun Run in the sun (but cold)', 'Da Trak'],
-            ['Camping Trip', '04-08-2018', '04-10-2018', '08:00:00', '22:00:00', 'Just friends and nature', 'nature'],
-            ['New Years Celebration', '12-31-2017', '01-01-2018', '20:00:00', '02:00:00', 'Celebrate the new year!', 'Around downtown']
+            ['Hiking', '11-07-2017', '11-07-2017', '08:00:00', '14:00:00', 'Hike before thanksgiving', 'pre-turkey hike', 'Outside Activities'],
+            ['Fun Run', '12-01-2017', '12-01-2017', '08:00:00', '15:00:00', 'A Fun Run in the sun (but cold)', 'Da Trak', 'Outside Activities'],
+            ['Camping Trip', '04-08-2018', '04-10-2018', '08:00:00', '22:00:00', 'Just friends and nature', 'nature', 'Outside Activities'],
+            ['New Years Celebration', '12-31-2017', '01-01-2018', '20:00:00', '02:00:00', 'Celebrate the new year!', 'Around downtown', 'Happy Fun Time']
         ];
 
-        $count = count($activities);
-
         foreach ($activities as $key => $activity) {
+            $group = $activities[1];
+            $group_id = Group::where('name' = $name)->pluck('id')->first();
+
+            $count = count($activities);
+
             Activity::insert([
                 'created_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
                 'updated_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
