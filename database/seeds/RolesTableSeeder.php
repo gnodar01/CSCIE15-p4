e65f1a0808692725b56d7ed0f8fa7efff350834f
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-11 22:19:13 -0500

    Add user info to roles table seed

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -13,21 +13,25 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         $roles = [
-            ['Guide', 'Know the trails, be the trails'],
-            ['Chef', 'Cook the food and feed us'],
-            ['Fire Starter', 'Only you can prevent forest fires'],
-            ['Musician', 'Bring your guitar!'],
-            ['Designated Driver', 'Or pay for the Uber']
+            ['Guide', 'Billy Bob', 'Know the trails, be the trails'],
+            ['Chef', 'Yin Yang', 'Cook the food and feed us'],
+            ['Fire Starter', 'Sally Sue', 'Only you can prevent forest fires'],
+            ['Musician', 'Yonny Yoman', 'Bring your guitar!'],
+            ['Designated Driver', 'Craig Church', 'Or pay for the Uber'],
         ];
 
         $count = count($roles);
         
         foreach ($roles as $key => $role) {
+            $username = $role[1];
+            $user_id = User::where('name', '=', $username)->pluck('id')->first();
+
             Role::insert([
                 'created_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
                 'updated_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
                 'name' => $role[0],
-                'description' => $role[1]
+                'user_id' => $user_id,
+                'description' => $role[2]
             ]);
             $count--;
         }
