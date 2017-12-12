91edc4a62531793a49923caef0209a99e0ba01df
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-11 22:04:18 -0500

    Create migration to connect activities and tasks

---
+++ database/migrations/2017_12_12_030232_connect_activities_and_tasks.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConnectActivitiesAndTasks extends Migration
+{
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->integer('activity_id')->unsigned();
+            $table->foreign('activity_id')->references('id')->on('activities');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            # tablename + fk field name + the word "foreign"
+            $table->dropForeign('tasks_activity_id_foreign');
+            $table->dropColumn('activity_id');
+        });
+    }
+}
