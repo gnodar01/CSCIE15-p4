e838380bc536254685069e05f833913a7c9b1b58
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-11 22:25:10 -0500

    Create migration to connect actvivites and roles

---
+++ database/migrations/2017_12_12_032414_connect_activities_and_roles.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConnectActivitiesAndRoles extends Migration
+{
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->integer('activity_id')->unsigned();
+            $table->foreign('activity_id')->references('id')->on('activities');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            # tablename + fk field name + the word "foreign"
+            $table->dropForeign('roles_activity_id_foreign');
+            $table->dropColumn('activity_id');
+        });
+    }
+}
