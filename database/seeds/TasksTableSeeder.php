a3ad329d1d1886ae40d88c50b2109f9dcdf8927b
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-11 22:05:02 -0500

    Add activity and user info to tasks table seed

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Task;
 use App\User;
+use App\Activity;
 
 class TasksTableSeeder extends Seeder
 {
@@ -14,15 +15,15 @@ class TasksTableSeeder extends Seeder
     public function run()
     {
         $tasks = [
-            ['Sneeks', 'Billy Bob', 'Get new sneakers'],
-            ['Watah', 'Sally Sue', 'Bring water'],
-            ['Playlist', 'Kelly Klarkson', 'Set up a new running playlist'],
-            ['Cooking Supplies', 'Yin Yang', 'Burner, grill, charcol/wood chips, lighter, tin foil'],
-            ['Food', 'Yonny Yoman', 'Steaks, Veggies'],
-            ['Alcohol', 'Billy Bob', 'Beer and wine'],
-            ['Champagne', 'Craig Church', 'Just in case ;)'],
-            ['Phone charger', 'Lucy Lee', 'Just in case'],
-            ['Poppers', 'Zak Zebra', 'Those things that shoot confetti when you pull the string']
+            ['Sneeks', 'Billy Bob', 'Fun Run', 'Get new sneakers'],
+            ['Watah', 'Sally Sue', 'Hiking', 'Bring water'],
+            ['Playlist', 'Kelly Klarkson', 'Fun Run', 'Set up a new running playlist'],
+            ['Cooking Supplies', 'Yin Yang', 'Camping Trip', 'Burner, grill, charcol/wood chips, lighter, tin foil'],
+            ['Food', 'Yonny Yoman', 'Camping Trip', 'Steaks, Veggies'],
+            ['Alcohol', 'Billy Bob', 'Camping Trip', 'Beer and wine'],
+            ['Champagne', 'Craig Church', 'New Years Celebration', 'Just in case ;)'],
+            ['Phone charger', 'Lucy Lee', 'New Years Celebration', 'Just in case'],
+            ['Poppers', 'Zak Zebra', 'New Years Celebration', 'Those things that shoot confetti when you pull the string']
         ];
 
         $count = count($tasks);
@@ -31,12 +32,16 @@ class TasksTableSeeder extends Seeder
             $username = $task[1];
             $user_id = User::where('name', '=', $username)->pluck('id')->first();
 
+            $activity = $task[2];
+            $activity_id = Activity::where('name', '=', $activity)->pluck('id')->first();
+
             Task::insert([
                 'created_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
                 'updated_at' => Carbon\Carbon::now()->subDays($count)->toDateTimeString(),
                 'name' => $task[0],
                 'user_id' => $user_id,
-                'description' => $task[2]
+                'activity_id' => $activity_id,
+                'description' => $task[3]
             ]);
             $count--;
         }
