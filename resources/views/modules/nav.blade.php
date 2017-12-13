7271bc0630d2c0efc84acb1ea2a389b935dd4352
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-13 00:47:39 -0500

    Setup conditional nav elements with auth

--- resources/views/modules/nav.blade.php
+++ resources/views/modules/nav.blade.php
@@ -7,13 +7,18 @@
 
 <nav>
     <ul>
-        <li><a href='/group'>All Groups</a></li>
-        @stack('nav')
-        <li>
-            <form method='POST' id='logout' action='/logout'>
-                {{csrf_field()}}
-                <a href='#' id="logout-link">Logout</a>
-            </form>
-        </li>
+        @if(Auth::check())
+            <li><a href='/group'>All Groups</a></li>
+            @stack('nav')
+            <li>
+                <form method='POST' id='logout' action='/logout'>
+                    {{csrf_field()}}
+                    <a href='#' id="logout-link">Logout</a>
+                </form>
+            </li>
+        @else
+            <li><a href='/register'>Register</a></li>
+            <li><a href='/login'>Login</a></li>
+        @endif
     </ul>
 </nav>
