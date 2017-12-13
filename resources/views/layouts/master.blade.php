c451d9613501ce784f5b357dde65997cc5f29d8d
Nodari Gogoberidze <nog267@g.harvard.edu>
2017-12-13 18:04:38 -0500

    Remove redundant charset attribute

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Nodari Gogoberidze">
-        <meta charset='utf-8'>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
         <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
